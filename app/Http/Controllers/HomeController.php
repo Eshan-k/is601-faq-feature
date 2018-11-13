01e3efb0e383847a3c71385e0d5fad31010ac138
Eshan-k <eshan.kulkarni15@gmail.com>
2018-11-12 19:51:47 -0500

    feature: added list of question on home page & pagination

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,11 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        return view('home');
-    }
+    
+	public function index()
+	{
+		$user = Auth::user();
+		$question = $user->question()->paginate(6);
+		return view('home')->with('question', $question);
+	}
 }
