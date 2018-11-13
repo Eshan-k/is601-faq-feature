c0a38a9df4776615e49cf53117f54638040c0a90
Eshan-k <eshan.kulkarni15@gmail.com>
2018-11-12 21:27:39 -0500

    feature: edited show method

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -45,7 +45,9 @@ class ProfileController extends Controller
      */
     public function show($id)
     {
-	   
+	    $user = Auth::user();
+	    $profile = $user->profile;
+	    return view('profile')->with('profile', $profile);
     }
 
     /**
